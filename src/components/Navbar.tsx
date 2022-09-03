29a37f650dfa47948a1fd39a3b0aada46c9c888e
iLoTep <jan.francksen@web.de>
2022-09-03 02:07:38 +0200

    Updated Header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='bg-lime-600 w-full fixed top-0 left-0 p-4'>
+        <div className='w-3/4 m-auto flex justify-between'>
+
+        <div className="">Logo</div>
+        <nav className='flex gap-x-2'>
+        <Link href="#"><a>Test</a></Link>
+        <Link href="#"><a>Test</a></Link>
+        <Link href="#"><a>Test</a></Link> 
+        </nav>
+        <button className='bg-white py-2 px-4'> test</button>
+        </div>
+    </header>
   )
 }
 
